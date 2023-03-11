09911da9317ec1d31453c0c0c7061135baf8af7b
Siyam <itsyakubsiyam@gmail.com>
2023-03-11 22:55:15 +0600

    test commit

--- rtk/index.js
+++ rtk/index.js
@@ -1,12 +1,12 @@
 const store = require("./app/store");
-const {fetchVideos} = require("./features/video/videoSlice");
+const { fetchVideos } = require("./features/video/videoSlice");
 
 // subscribe to state changes
 store.subscribe(() => {
-    // console.log(store.getState());
+  // console.log(store.getState());
 });
 
 // disptach actions
 store.dispatch(fetchVideos());
 
-console.log(fetchVideos());
+console.log(`Hello ${fetchVideos()?.video?.videos}`);
